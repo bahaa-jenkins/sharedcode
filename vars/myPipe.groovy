e6ba988f9d13a91199867fdfc8a87faccde95ad1
Ahmed_Bahaa <34777487+ahmed-bahaa@users.noreply.github.com>
2019-10-29 17:00:29 +0200

    Update myPipe.groovy

--- vars/myPipe.groovy
+++ vars/myPipe.groovy
@@ -2,6 +2,9 @@ def call( Map settings = [:] ) {
     node('master'){
            stage('build') {
                sh 'echo Building ${BRANCH_NAME}...'
-     }
+                         }
+            stage('test') {
+               sh 'echo Building ${JOB_NAME}...'
+                         }
     }
 }
